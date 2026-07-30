e7a4a36677630997691dd916d5ad3b6034540d64
Ange Carolle <y.carolle07@gmail.com>
2026-07-30 12:27:00 +0200

    Ajout d'un projet

--- src/pages/Portfolio3.jsx
+++ src/pages/Portfolio3.jsx
@@ -182,7 +182,7 @@ export default function Portfolio3() {
     {
       titre: "Expenses Tracker App",
       desc: "Application web de suivi des dépenses.",
-      techs: ["React Native", "django", "Tailwind CSS"],
+      techs: ["React js", "django", "Tailwind CSS"],
       image: Expenses,
       status: "Terminé",
       featured: false,
